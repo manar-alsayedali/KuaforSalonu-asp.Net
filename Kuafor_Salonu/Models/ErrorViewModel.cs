a0477a30c33b93fb0b1fe515a1b7f3988838ce41
manar <menarmms9@gmail.com>
2024-12-16 16:40:44 +0300

    init

---
+++ Kuafor_Salonu/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Kuafor_Salonu.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
